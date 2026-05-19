3b02b9e445f87cc459a863417f12c94dc4661c97
Subhant Tomar <subhanttom002@gmail.com>
2026-05-20 00:51:32 +0530

    Update base URL to Railway deployment

--- app/src/main/java/com/trackit/trackit/network/RetrofitInstance.kt
+++ app/src/main/java/com/trackit/trackit/network/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "https://trackit-backend-production.up.railway.app/"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
